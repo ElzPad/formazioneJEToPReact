2b1d10e32fcc92b7b8d18b502220ee6230d9456b
ElziarioPaduano <e.paduano@jetop.com>
2024-04-12 15:16:36 +0200

    05. FetchExample - Correct: useEffect

--- src/components/molecules/JEList.jsx
+++ src/components/molecules/JEList.jsx
@@ -1,17 +1,19 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function JEList() {
   const [url, setUrl] = useState("http://localhost:3000/JuniorEnterprises");
   const [data, setData] = useState();
 
-  fetch(url).then((Response) => {
-    return Response.json();
-  }).then((jsonData) => {
-    setData(jsonData);
-    console.log(data);
-  }).catch(() => {
-    console.log("error while fetching");
-  })
+  useEffect(() => {
+    fetch(url).then((Response) => {
+      return Response.json();
+    }).then((jsonData) => {
+      setData(jsonData);
+      console.log(jsonData);
+    }).catch(() => {
+      console.log("error while fetching");
+    })
+  }, []);
 
   return (
     <>
